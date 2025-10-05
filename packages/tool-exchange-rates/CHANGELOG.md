c6c14d0c82c3485e155bbd53461985c0522cecc0
Gokcen Ogutcu <gokcen@brewww.com>
2025-10-05 13:13:05 +0300

    feat: Implemented real-time data fetching into Exchange Rates Tool.

---
+++ packages/tool-exchange-rates/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @sufle/tool-exchange-rates
+
+## 0.1.0
+
+### Minor Changes
+
+- Implemented real-time data fetching into Exchange Rates Tool.
--- packages/tool-exchange-rates/src/index.ts
+++ packages/tool-exchange-rates/src/index.ts
@@ -1,21 +1,43 @@
 import { z } from "zod";
+import { fetchCurrency } from "kurlar";
 
 const name: string = "exchangeRates";
-const description: string = `Get exchange rate information for input currency. When asked about exchange rates for ANY currency, you MUST call the "exchangeRates" tool with that currency name.`;
+const description: string = `Get exchange rate information for input currency. When asked about exchange rates for ANY currency, you MUST call the "exchangeRates" tool with the 3-letter currency code.`;
 
 const create = (opts) => {
   const schema = z.object({
     currency: z
-      .string()
-      .describe("Name of the currency to get the exchange rate for."),
+      .enum([
+        "USD",
+        "AUD",
+        "DKK",
+        "EUR",
+        "GBP",
+        "CHF",
+        "SEK",
+        "CAD",
+        "KWD",
+        "NOK",
+        "SAR",
+        "JPY",
+        "BGN",
+        "RON",
+        "RUB",
+        "CNY",
+        "PKR",
+        "QAR",
+        "KRW",
+        "AZN",
+        "AED",
+      ])
+      .describe("3-letter code of the currency to get the exchange rate for."),
   });
   const provider = async (input: any) => {
     const { currency } = schema.parse(input);
-    return {
-      sourceCurrency: currency,
-      targetCurrency: "TRL",
-      exchangeRate: 47.5,
-    };
+    const result = await fetchCurrency({
+      currency,
+    });
+    return result;
   };
   return { provider, schema, name, description };
 };

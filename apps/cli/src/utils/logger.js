bf235f8ef22dbd2541e04f099f3d9e0624950137
Gokcen Ogutcu <gokcen@brewww.com>
2025-12-31 00:05:46 +0300

    Update logger.js

--- apps/cli/src/utils/logger.js
+++ apps/cli/src/utils/logger.js
@@ -1,14 +1,14 @@
 import { createLogger, format, transports } from "winston";
 
 const logger = createLogger({
-  level: process.env.LOG_LEVEL || "info",
+  level: process.env.LOG_LEVEL || "debug",
   format: format.combine(
     format.timestamp({
       format: "YYYY-MM-DD HH:mm:ss",
     }),
     format.errors({ stack: true }),
     format.splat(),
-    format.json(),
+    format.json()
   ),
   transports: [
     new transports.Console({
